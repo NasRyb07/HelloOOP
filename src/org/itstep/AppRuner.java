e0b8592297b930408675fcc8310b52e5cd35e074
NasRyb07 <Nastya_07@bigmir.net>
2019-05-23 21:10:48 +0300

    Add new clases

---
+++ src/org/itstep/AppRuner.java
@@ -0,0 +1,18 @@
+package org.itstep;
+
+public class AppRuner {
+}
+
+    public static void main(String[] args) {
+        User me = new User ();
+        me.name = "Anastasiiya";
+        me.pasvord = "7777777";
+
+                User you = new User ();
+                you.name = "Your name";
+                you.pasvord = "your pass";
+
+
+                System.out.println ("User ME has name")
+
+    }
---
+++ src/org/itstep/User.java
@@ -0,0 +1,7 @@
+package org.itstep;
+
+public class User {
+    String name;
+    String pasvord;
+
+}
